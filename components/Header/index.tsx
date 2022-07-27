efc6320c2821a4bfe5cc51549b8e9243164f1ec9
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-07-26 21:45:37 -0300

    [ FEATURE ] Creating component Header.

---
+++ components/Header/index.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+
+import styles from "./styles.module.scss";
+
+interface Link {
+  id: string;
+  text: string;
+  href: string;
+  actived: boolean;
+  [key: string]: any;
+}
+
+interface HeaderProps {
+  links: Link[];
+}
+
+export function Header({ links }: HeaderProps) {
+  return (
+    <header className={styles.header}>
+      <div className="container">
+        <nav className={styles.nav}>
+          <ul>
+            {links.map((link: Link) => (
+              <li
+                key={link.id}
+                className={`${styles.link} ${link.actived && styles.actived}`}
+              >
+                <a href={link.id}>{link.text}</a>
+              </li>
+            ))}
+          </ul>
+
+          <a
+            href="/data/resume.pdf"
+            target="_blank"
+            className="container-image"
+          >
+            <Image
+              src="/avatar.png"
+              alt="Avatar"
+              className="avatar"
+              width={100}
+              height={93}
+            />
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,40 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import { Header } from "../components/Header";
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Portfolio - Wevison</title>
         <meta name="description" content="Porfolio - Wevison R." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>Hello World!</h1>
-      </main>
+      <Header
+        links={[
+          {
+            id: "link-01",
+            text: "About Me",
+            href: "#section-01",
+            actived: true,
+          },
+          { id: "link-02", text: "Work", href: "#section-02", actived: false },
+          {
+            id: "link-03",
+            text: "Formation",
+            href: "#section-03",
+            actived: false,
+          },
+          {
+            id: "link-04",
+            text: "Contact",
+            href: "#section-04",
+            actived: false,
+          },
+        ]}
+      />
     </>
   );
 };
