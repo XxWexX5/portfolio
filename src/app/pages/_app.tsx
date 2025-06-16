78b500adff3b25bf546cdf9c495c4092cdfcbfa5
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-06-16 18:58:40 -0300

    [ FEATURE ] Deploy.

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "@/lib/apollo-client";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: any) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
