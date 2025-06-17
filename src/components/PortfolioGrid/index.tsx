255667d5e8205e50f7ce65d34273bf8a96b70201
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-06-16 22:29:14 -0300

    [ REFACT ] Adjust layout.

--- src/components/PortfolioGrid/index.tsx
+++ src/components/PortfolioGrid/index.tsx
@@ -27,19 +27,13 @@ export default function PortfolioGrid({ galleries }: PortfolioGridProps) {
   return (
     <div className="grid grid-cols-1 grid-rows-3 sm:grid-cols-2 lg:grid-cols-3">
       {galleries.map(({ gallery }, index) => {
-        {
-          console.log(gallery);
-        }
-
         return (
           <div key={index} className="overflow-hidden">
             <Link
               href={gallery[0].link}
               target="_blank"
               rel="noopener noreferrer"
-              className={`relative block h-[32rem] hover:scale-105 transition-transform duration-300 ${
-                index > 2 && "hidden lg:block"
-              }`}
+              className={`relative block h-[32rem] hover:scale-105 transition-transform duration-300`}
             >
               <Image
                 src={gallery[0].image.url}
